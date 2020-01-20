00ff69029da31d2284a1bfe69bb670b3d10df13b
imartha <master.amarta@gmail.com>
2020-01-21 02:29:43 +0700

    about section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $('.carousel').carousel({
     interval: 4000,
     pause: false, //ketika di kursor di atas, bisa hover atau false
-    wrap: true, //looping kembali ke awal
+    wrap: false, //looping kembali ke awal
     touch: true
 })
