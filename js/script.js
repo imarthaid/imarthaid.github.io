b1b503324493c125b97d4f04b9b71677eeb229d8
imartha <master.amarta@gmail.com>
2020-01-23 04:02:07 +0700

    mobile version

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $('.page-scroll').on('click', function (e) {
     // console.log($('html, body').scrollTop())
 
     $('html , body').animate({
-        scrollTop: elemenTujuan.offset().top - 50
+        scrollTop: elemenTujuan.offset().top - 72
     }, 1000, 'swing');
 
     e.preventDefault();
